70a4f40b5d58d4a7535eb06b6456eefc63fa993d
Shaun Bokenham <shaun@3dmarcomms.com>
2018-06-12 10:45:11 +0100

    First step, add basic required validation.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Box;
+use Illuminate\Http\Request;
 
 /**
  * Class OrderController
@@ -13,13 +14,21 @@ class OrderController extends Controller
     public function index()
     {
         $boxes = Box::all();
+
         return view('order', compact('boxes'));
     }
 
-    public function save()
+    public function save(Request $request)
     {
         // validate input (view needs to display errors)
-
+        $this->validate($request, [
+            'first_name' => 'required',
+            'surname' => 'required',
+            'email' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+            'postcode' => 'required',
+        ]);
         // use CreateOrder service to dispatch order to the warehouse
 
         // store order and order items in the database with Models provided
